72cfdecf958589a8bdd0ea5fe7115b696f680a44
orochimeii <babymeyfong@gmail.com>
2022-06-13 16:48:47 -0300

    initial commit

---
+++ TP-Final_Grupo3/src/main/java/ar/edu/unju/fi/TpFinalGrupo3Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpFinalGrupo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpFinalGrupo3Application.class, args);
+	}
+
+}
---
+++ TP-Final_Grupo3/src/test/java/ar/edu/unju/fi/TpFinalGrupo3ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpFinalGrupo3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
