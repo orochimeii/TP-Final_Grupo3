714c04ef794e876a0b16d5aa5460777a88511ece
orochimeii <babymeyfong@gmail.com>
2022-06-19 15:06:44 -0300

    entity empleador y oferta

--- TP-Final_Grupo3/src/main/java/ar/edu/unju/fi/entity/Ciudadano.java
+++ TP-Final_Grupo3/src/main/java/ar/edu/unju/fi/entity/Ciudadano.java
@@ -1,5 +1,80 @@
 package ar.edu.unju.fi.entity;
 
-public class Ciudadano {
+import java.time.LocalDate;
 
+public class Ciudadano {
+	private int dni;
+	private int numeroDeTramite;
+	private String email;
+	private String estadoCivil;
+	private String privincia;
+	private String telefono;
+	private LocalDate fechaDeNacimiento;
+	private String consenia;
+	
+	public Ciudadano(int dni, int numeroDeTramite, String email, String estadoCivil, String privincia, String telefono,
+			LocalDate fechaDeNacimiento, String consenia) {
+		super();
+		this.dni = dni;
+		this.numeroDeTramite = numeroDeTramite;
+		this.email = email;
+		this.estadoCivil = estadoCivil;
+		this.privincia = privincia;
+		this.telefono = telefono;
+		this.fechaDeNacimiento = fechaDeNacimiento;
+		this.consenia = consenia;
+	}
+	public Ciudadano() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public int getDni() {
+		return dni;
+	}
+	public void setDni(int dni) {
+		this.dni = dni;
+	}
+	public int getNumeroDeTramite() {
+		return numeroDeTramite;
+	}
+	public void setNumeroDeTramite(int numeroDeTramite) {
+		this.numeroDeTramite = numeroDeTramite;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getEstadoCivil() {
+		return estadoCivil;
+	}
+	public void setEstadoCivil(String estadoCivil) {
+		this.estadoCivil = estadoCivil;
+	}
+	public String getPrivincia() {
+		return privincia;
+	}
+	public void setPrivincia(String privincia) {
+		this.privincia = privincia;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	public LocalDate getFechaDeNacimiento() {
+		return fechaDeNacimiento;
+	}
+	public void setFechaDeNacimiento(LocalDate fechaDeNacimiento) {
+		this.fechaDeNacimiento = fechaDeNacimiento;
+	}
+	public String getConsenia() {
+		return consenia;
+	}
+	public void setConsenia(String consenia) {
+		this.consenia = consenia;
+	}
 }
--- TP-Final_Grupo3/src/main/java/ar/edu/unju/fi/entity/Oferta.java
+++ TP-Final_Grupo3/src/main/java/ar/edu/unju/fi/entity/Oferta.java
@@ -1,5 +1,179 @@
 package ar.edu.unju.fi.entity;
 
-public class Oferta {
+import java.util.ArrayList;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Entity
+@Table(name = "ofertas")
+public class Oferta {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID")
+	private Long id;
+	
+	@Min(value=1, message="El valor mínimo es 1")
+	@Max(value=9999,message="El valor máximo permitido es 9999")
+	@Column(name = "VACANTES")
+	private int cantidadDeVacantes;
+	
+	@NotEmpty(message="Este campo no puede ser vacío")
+	@Column(name = "PUESTO")
+	private String puestoRequerido;
+	
+	@NotEmpty(message="Este campo no puede ser vacío")
+	@Column(name = "RESUMEN")
+	private String resumenPuesto;
+	
+	@NotEmpty(message="Este campo no puede ser vacío")
+	@Column(name = "DISPONIBILIDAD")
+	private String disponibilidad;
+	
+	@Column(name = "TAREAS")
+	private ArrayList <String> tareasPrincipales;
+	
+	@NotEmpty(message="Este campo no puede ser vacío")
+	@Column(name = "CONTACTO")
+	private ArrayList <String> datosContacto;
+	
+	@NotEmpty(message="Este campo no puede ser vacío")
+	@Column(name = "JORNADA")
+	private String jornada;
+	
+	@NotEmpty(message="Este campo no puede ser vacío")
+	@Column(name = "REQUISITOS")
+	private String requisitos;
+	
+	@Column(name = "SALARIO")
+	private double salario;
+	
+	@Column(name = "BENEFICIOS")
+	private String beneficios;
+	
+	@NotEmpty(message="Este campo no puede ser vacío")
+	@Column(name = "DISPONIBLE")
+	private boolean disponible;
+	
+	@Autowired
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "EMPLEADOR_ID")
+	private Empleador empleador;
+	
+	public Oferta(int cantidadDeVacantes, String puestoRequerido, String resumenPuesto, String disponibilidad,
+			ArrayList<String> tareasPrincipales, ArrayList<String> datosContacto, String jornada, String requisitos,
+			double salario, String beneficios, boolean disponible) {
+		super();
+		this.cantidadDeVacantes = cantidadDeVacantes;
+		this.puestoRequerido = puestoRequerido;
+		this.resumenPuesto = resumenPuesto;
+		this.disponibilidad = disponibilidad;
+		this.tareasPrincipales = tareasPrincipales;
+		this.datosContacto = datosContacto;
+		this.jornada = jornada;
+		this.requisitos = requisitos;
+		this.salario = salario;
+		this.beneficios = beneficios;
+		this.disponible = disponible;
+	}
+	
+	public Oferta() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public int getCantidadDeVacantes() {
+		return cantidadDeVacantes;
+	}
+	public void setCantidadDeVacantes(int cantidadDeVacantes) {
+		this.cantidadDeVacantes = cantidadDeVacantes;
+	}
+	public String getPuestoRequerido() {
+		return puestoRequerido;
+	}
+	public void setPuestoRequerido(String puestoRequerido) {
+		this.puestoRequerido = puestoRequerido;
+	}
+	public String getResumenPuesto() {
+		return resumenPuesto;
+	}
+	public void setResumenPuesto(String resumenPuesto) {
+		this.resumenPuesto = resumenPuesto;
+	}
+	public String getDisponibilidad() {
+		return disponibilidad;
+	}
+	public void setDisponibilidad(String disponibilidad) {
+		this.disponibilidad = disponibilidad;
+	}
+	public ArrayList<String> getTareasPrincipales() {
+		return tareasPrincipales;
+	}
+	public void setTareasPrincipales(ArrayList<String> tareasPrincipales) {
+		this.tareasPrincipales = tareasPrincipales;
+	}
+	public ArrayList<String> getDatosContacto() {
+		return datosContacto;
+	}
+	public void setDatosContacto(ArrayList<String> datosContacto) {
+		this.datosContacto = datosContacto;
+	}
+	public String getJornada() {
+		return jornada;
+	}
+	public void setJornada(String jornada) {
+		this.jornada = jornada;
+	}
+	public String getRequisitos() {
+		return requisitos;
+	}
+	public void setRequisitos(String requisitos) {
+		this.requisitos = requisitos;
+	}
+	public double getSalario() {
+		return salario;
+	}
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+	public String getBeneficios() {
+		return beneficios;
+	}
+	public void setBeneficios(String beneficios) {
+		this.beneficios = beneficios;
+	}
+	public boolean isDisponible() {
+		return disponible;
+	}
+	public void setDisponible(boolean disponible) {
+		this.disponible = disponible;
+	}
+
+	public Empleador getEmpleador() {
+		return empleador;
+	}
+
+	public void setEmpleador(Empleador empleador) {
+		this.empleador = empleador;
+	}
 }
