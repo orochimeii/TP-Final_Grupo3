6b27b0f6b58c48711e3c7a34ffb01669ded09e80
maxi1 <maxi1@DESKTOP-G09S1M5>
2022-06-29 14:44:34 -0300

    Curriculum 1.1

--- TP-Final_Grupo3/src/main/java/ar/edu/unju/fi/controller/CvController.java
+++ TP-Final_Grupo3/src/main/java/ar/edu/unju/fi/controller/CvController.java
@@ -3,9 +3,15 @@ package ar.edu.unju.fi.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import ar.edu.unju.fi.entity.CV;
 import ar.edu.unju.fi.service.ICVService;
 
 @Controller
@@ -22,6 +28,16 @@ public class CvController {
 		return "crear_cv";
 	}
 	
-	
+	@PostMapping("")
+	public ModelAndView ahre(@Validated @ModelAttribute("cvAlias") CV cv, BindingResult bindingResult) {
+		if(bindingResult.hasErrors()) {
+			ModelAndView modeloVista = new ModelAndView("crear_cv");
+			modeloVista.addObject("cvAlias", cv);
+			return modeloVista;
+		}
+		
+		ModelAndView modeloVista = new ModelAndView("redirect:/login");
+		return modeloVista;
+	}
 	
 }
