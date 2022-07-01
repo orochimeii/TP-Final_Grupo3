92201081ff58b88bf3132974bad056a40b23f516
maxi1 <maxi1@DESKTOP-G09S1M5>
2022-07-01 19:37:26 -0300

    Mejoras

--- TP-Final_Grupo3/src/main/java/ar/edu/unju/fi/service/ICursoService.java
+++ TP-Final_Grupo3/src/main/java/ar/edu/unju/fi/service/ICursoService.java
@@ -6,6 +6,7 @@ import ar.edu.unju.fi.entity.Curso;
 
 public interface ICursoService {
 	
+	public void agregarCurso(Curso curso);
 	public List<Curso> obtenerCursos();
 	public Curso buscarCurso(Long id);
 }
