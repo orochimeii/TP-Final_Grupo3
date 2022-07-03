0f625111f29b0c961b72bc5b4772f33627cef4b4
orochimeii <babymeyfong@gmail.com>
2022-07-03 14:10:14 -0300

    un poco de athorization

--- TP-Final_Grupo3/src/main/java/ar/edu/unju/fi/controller/OfertaController.java
+++ TP-Final_Grupo3/src/main/java/ar/edu/unju/fi/controller/OfertaController.java
@@ -1,13 +1,60 @@
 package ar.edu.unju.fi.controller;
 
+import java.security.Principal;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import ar.edu.unju.fi.entity.Empleador;
+import ar.edu.unju.fi.entity.Oferta;
+import ar.edu.unju.fi.service.IEmpleadorService;
+import ar.edu.unju.fi.service.IOfertaService;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 @Controller
 public class OfertaController {
 	
-@GetMapping("/ofertas")
-public String getOfertaPage() {
-	return "lista_ofertas";
-}
+	private static final Log LOGGER = LogFactory.getLog(OfertaController.class);
+
+	@Autowired
+	private IOfertaService ofertaService;
+	
+	@Autowired
+	private IEmpleadorService empleadorService;
+	
+	@GetMapping("/ofertas")
+	public ModelAndView getOfertaPage(Model model) {
+		ModelAndView mav = new ModelAndView("lista_ofertas");
+		mav.addObject("ofertas", ofertaService.getOfertas());
+		return mav;
+	}
+	
+	@GetMapping("/crear_oferta")
+	public String getCrearOfertaPage(Model model) {
+		model.addAttribute("oferta", new Oferta());
+		model.addAttribute("empleador", new Empleador());
+		return "crear_ofertas";
+	}
+	
+	@PostMapping("/crear_oferta")
+	public String registro(@Valid @ModelAttribute Oferta oferta, Principal principal, BindingResult result, Model model) {
+		LOGGER.info(principal.getName());
+		if(result.hasErrors()) {
+			return "redirect:/crear_oferta";
+		}else {
+			Empleador empleador = empleadorService.findByCuit(principal.getName());
+			oferta.setEmpleador(empleador);
+			ofertaService.crear(oferta);
+		}
+		return "redirect:/ofertas";
+	}
 }
