bdbdf145baea3c7737d0d899bc0ee53e3d3fa019
maxi1 <maxi1@DESKTOP-G09S1M5>
2022-07-04 17:13:56 -0300

    LOG

--- TP-Final_Grupo3/src/main/java/ar/edu/unju/fi/controller/CursoController.java
+++ TP-Final_Grupo3/src/main/java/ar/edu/unju/fi/controller/CursoController.java
@@ -2,6 +2,8 @@ package ar.edu.unju.fi.controller;
 
 import java.time.LocalDate;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
@@ -20,15 +22,23 @@ import ar.edu.unju.fi.service.ICursoService;
 @RequestMapping("/curso")
 public class CursoController {
 	
+	private static final Log LOGGER = LogFactory.getLog(CursoController.class);
+	
 	@Autowired
 	ICursoService cursoService;
 	
 	@Autowired
 	private ICiudadanoService ciudadanoService;
 	
+	
+	/*El siguiente modulo nos permite crear los cursos como listarlos
+	 * Si no existen cursos, los crea y luego los muestras. Si ya hay cursos cargados simplemente los muestra
+	 * */
 	@GetMapping("/listaCursos")
 	public String verCursos(Model model) {
+		LOGGER.info("Listar cursos");
 		if(cursoService.obtenerCursos().isEmpty()) {
+			LOGGER.info("No existen cursos, por lo cual se crean");
 			cursoService.agregarCurso(new Curso("Full Stack en Java", "Programación", LocalDate.of(2022, 8, 25), LocalDate.of(2022, 9, 25), 32, "Virtual", "Daniel Osvaldo"));
 			cursoService.agregarCurso(new Curso("Adobe Photoshop", "Diseño Gráfico", LocalDate.of(2022, 8, 1), LocalDate.of(2022, 8, 31), 20, "Virtual", "Pablo Ledesma"));
 			cursoService.agregarCurso(new Curso("Mecanografía", "Informática", LocalDate.of(2022, 7, 5), LocalDate.of(2022, 7, 9), 8, "Presencial", "Cristian Calleri"));
@@ -38,6 +48,10 @@ public class CursoController {
 		return "lista_cursos";
 	}
 	
+	/*El siguiente modulo nos inscribir al usuario loggeado en un curso seleccionado
+	 * Busca entre todos los cursos y verifica que no este ya inscripto
+	 * En caso de estarlo, simplemente se lo redirecciona a la lista de sus cursos
+	 * */
 	@GetMapping("/inscribirse/{id}")
 	public ModelAndView inscribirse(@PathVariable(value="id") Long id, Authentication authentication) {
 		Ciudadano ciudadano = ciudadanoService.findByDni(authentication.getName());
@@ -50,20 +64,25 @@ public class CursoController {
 		}
 		
 		if(inscripto==false) {
+			LOGGER.info("Un ciudadano se inscribe a un curso");
 			ModelAndView modeloVista = new ModelAndView("redirect:/curso/cursos");
 			Curso curso = cursoService.buscarCurso(id);
 			ciudadano.getCursos().add(curso);
 			cursoService.agregarCurso(curso);
 			return modeloVista;
 		}else {
+			LOGGER.info("El ciudadano ya esta inscripto");
 			ModelAndView modeloVista = new ModelAndView("redirect:/curso/cursos");
 			return modeloVista;
 		}
 		
 	}
 	
+	/*El siguiente modulo permite al usuario loggeado ver los cursos donde esta registrado
+	 * */
 	@GetMapping("/cursos")
 	public String verMisCursos(Authentication authentication, Model model){
+		LOGGER.info("Ver los cursos de: "+authentication.getName());
 		Ciudadano ciudadano = ciudadanoService.findByDni(authentication.getName());
 		model.addAttribute("vacio", ciudadano.getCursos().isEmpty());
 		model.addAttribute("cursosAlias", ciudadano.getCursos());
--- TP-Final_Grupo3/src/main/java/ar/edu/unju/fi/controller/CvController.java
+++ TP-Final_Grupo3/src/main/java/ar/edu/unju/fi/controller/CvController.java
@@ -32,6 +32,9 @@ public class CvController {
 	@Autowired
 	ICiudadanoService ciudadanoService;
 	
+	/*
+	 * Estamos creando un CV para un ciudadano, si ya tiene CV directamente nos manda a verlo
+	 * */
 	@GetMapping("/crear")
 	public String getFormularioPage(Model model, Authentication authentication) {
 		
@@ -47,9 +50,13 @@ public class CvController {
 		
 	}
 	
+	/*
+	 * Valida que el CV este correctamente y lo guarda
+	 * */
 	@PostMapping("/crear")
 	public ModelAndView cargarCurriculo(@Validated @ModelAttribute("cvAlias") CV cv, BindingResult bindingResult, Authentication authentication) {
 		if(bindingResult.hasErrors()) {
+			LOGGER.info("CV con datos incorrectos");
 			ModelAndView modeloVista = new ModelAndView("crear_cv");
 			modeloVista.addObject("cvAlias", cv);
 			return modeloVista;
@@ -67,6 +74,9 @@ public class CvController {
 		return modeloVista;
 	}
 	
+	/*
+	 * Nos permite editar el CV del usuario loggeado y en caso de no haber creado el cv lo redirige hacia ahi
+	 * */
 	@GetMapping("/editar")
 	public String editarCV(Model model, Authentication authentication) {
 		Ciudadano ciudadano = ciudadanoService.findByDni(authentication.getName());
@@ -81,9 +91,13 @@ public class CvController {
 		
 	}
 	
+	/*
+	 * Nos permite verificar y actualizar el CV del usuario loggeado 
+	 * */
 	@PostMapping("/editar")
 	public ModelAndView editar(@Validated @ModelAttribute("cvAlias") CV cv, BindingResult bindingResult, Authentication authentication) {
 		if(bindingResult.hasErrors()) {
+			LOGGER.info("CV con datos incorrectos");
 			ModelAndView modeloVista = new ModelAndView("editar_cv");
 			modeloVista.addObject("cvAlias", cv);
 			return modeloVista;
@@ -96,6 +110,9 @@ public class CvController {
 		return modeloVista;
 	}
 	
+	/*
+	 * Nos permite ver el CV del usuario loggeado
+	 * */
 	@GetMapping("/ver")
 	public String verCV(Model model, Authentication authentication) {
 		LOGGER.info("Ver CV - "+authentication.getName());
