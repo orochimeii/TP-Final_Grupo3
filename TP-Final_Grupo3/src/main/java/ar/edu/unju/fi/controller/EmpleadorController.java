74ea03b2c24c5e51f12cb429f3b1f912525df73b
maxi1 <maxi1@DESKTOP-G09S1M5>
2022-07-03 21:52:39 -0300

    Subida de todo

--- TP-Final_Grupo3/src/main/java/ar/edu/unju/fi/controller/EmpleadorController.java
+++ TP-Final_Grupo3/src/main/java/ar/edu/unju/fi/controller/EmpleadorController.java
@@ -2,10 +2,14 @@ package ar.edu.unju.fi.controller;
 
 import javax.validation.Valid;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +22,8 @@ import ar.edu.unju.fi.service.IEmpleadorService;
 @Controller
 @RequestMapping("/empleador")
 public class EmpleadorController {
+	
+	private static final Log LOGGER = LogFactory.getLog(EmpleadorController.class);
 	
 	@Autowired
 	private IEmpleadorService empleadorService;
@@ -45,5 +51,34 @@ public class EmpleadorController {
 			empleadorService.registrar(empleador);
 		}
 		return "redirect:/login/empleador";
+	}
+	
+	@GetMapping("/ver")
+	public String verEmpleador(Model model, Authentication authentication) {
+		LOGGER.info(authentication.getName());
+		Empleador empleador = empleadorService.findByCuit(authentication.getName());
+		model.addAttribute("empleador", empleador);
+		return "ver_empleador";
+	}
+	
+	@GetMapping("/editar")
+	public String editarEmpleador(Model model, Authentication authentication) {
+		LOGGER.info(authentication.getName());
+		Empleador empleador = empleadorService.findByCuit(authentication.getName());
+		model.addAttribute("empleador", empleador);
+		return "editar_empleador";
+	}
+	
+	@PostMapping("/editar")
+	public ModelAndView editar(@Validated @ModelAttribute("empleador") Empleador empleador, BindingResult bindingResult, Authentication authentication) {
+		if(bindingResult.hasErrors()) {
+			ModelAndView modeloVista = new ModelAndView("editar_empleador");
+			modeloVista.addObject("empleador", empleador);
+			return modeloVista;
+		}
+		
+		ModelAndView modeloVista = new ModelAndView("redirect:/inicio");
+		empleadorService.registrar(empleador);
+		return modeloVista;
 	}
 }
